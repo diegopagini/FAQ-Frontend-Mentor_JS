380e37c6c8615a2c0e3a3c347cdb1cc63a97e9d0
unknown <diegopagini777@gmail.com>
2020-10-10 12:37:56 -0300

    desktop ready

---
+++ index.js
@@ -0,0 +1,15 @@
+let arrows = document.getElementsByClassName('arrow');
+for (let i = 0; i <= arrows.length; i++) {
+  arrows[i].addEventListener("click", function(){
+    console.log("Me estan haciendo click")
+  })
+}
+
+
+let answer1 = document.getElementById('answer-1');
+let arrow1 = document.getElementById('arrow-1"');
+
+arrow1.addEventListener("click", function(){
+  answer1.style.display = "block";
+  console.log("hola");
+});
