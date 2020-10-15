0bd23f39c30a0e0de4a0d4850d2d5c5d25eb5857
diegopagini <diegopagini777@gmail.com>
2020-10-15 06:51:07 -0300

    algo no esta bien

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-let arrows = document.getElementsByClassName('arrow');
-let answers = document.getElementsByClassName('answer');
-for (let i = 0; i < arrows.length; i++) {
-  arrows[i].addEventListener("click", function(){
+let arrows = document.getElementsByClassName("arrow");
+let answers = document.getElementsByClassName("answer");
 
+for (let i = 0; i < arrows.length; i++) {
+  arrows[i].addEventListener("click", function () {
     let currentAnswer = answers[i].style.display;
 
-    if (currentAnswer === '' || currentAnswer === 'none') {
-      currentAnswer = 'block'
+    if (currentAnswer === "" || currentAnswer === "none") {
+      currentAnswer = "block";
     } else {
-      currentAnswer = 'none'
+      currentAnswer = "none";
     }
-
-  })
+  });
 }
